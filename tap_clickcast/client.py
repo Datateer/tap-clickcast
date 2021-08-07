07a6553ba0f8d43abb652d32eb57d5c8aa3075af
Adam Roderick <aroder@gmail.com>
2021-08-07 12:31:51 -0600

    initial commit. Build with the Meltano Singer SDK. Has some initial tests. Is able to pull down all the Employers

---
+++ tap_clickcast/client.py
@@ -0,0 +1,93 @@
+"""REST client handling, including ClickcastStream base class."""
+
+import requests
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+from tap_clickcast.auth import clickcastAuthenticator
+
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+
+class ClickcastStream(RESTStream):
+    """clickcast stream class."""
+
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return self.config["api_url_base"]
+
+    records_jsonpath = "$.results[*]"  # "$[*]"  # Or override `parse_response`.
+    current_page_jsonpath = "$.page"
+    page_count_jsonpath = "$.num_pages"
+    # next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
+
+    @property
+    def authenticator(self) -> clickcastAuthenticator:
+        """Return a new authenticator object."""
+        return clickcastAuthenticator.create_for_stream(self)
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        if "user_agent" in self.config:
+            headers["User-Agent"] = self.config.get("user_agent")
+        # If not using an authenticator, you may also provide inline auth headers:
+        # headers["Private-Token"] = self.config.get("auth_token")
+        return headers
+
+    def get_current_page(self, response: requests.Response):
+        matches = extract_jsonpath(self.current_page_jsonpath, response.json())
+        current_page = next(iter(matches), None)
+        return current_page
+
+    def get_page_count(self, response: requests.Response):
+        matches = extract_jsonpath(self.page_count_jsonpath, response.json())
+        page_count = next(iter(matches), None)
+        return page_count
+
+    def get_next_page_token(self, response: requests.Response, previous_token: Optional[Any]) -> Optional[Any]:
+        """Return a token for identifying next page or None if no more pages."""
+        # TODO: If pagination is required, return a token which can be used to get the
+        #       next page. If this is the final page, return "None" to end the
+        #       pagination loop.
+        current = self.get_current_page(response)
+        count = self.get_page_count(response)
+        if current == count:
+            next_page_token = None
+        elif current < count:
+            next_page_token = current + 1
+
+        return next_page_token
+
+    def get_url_params(self, context: Optional[dict], next_page_token: Optional[Any]) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["sort"] = "asc"
+            params["order_by"] = self.replication_key
+        return params
+
+    def prepare_request_payload(self, context: Optional[dict], next_page_token: Optional[Any]) -> Optional[dict]:
+        """Prepare the data payload for the REST API request.
+
+        By default, no payload will be sent (return None).
+        """
+        # TODO: Delete this method if no payload is required. (Most REST APIs.)
+        return None
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        # TODO: Parse response body and return a set of records.
+        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
+
+    def post_process(self, row: dict, context: Optional[dict]) -> dict:
+        """As needed, append or transform raw data to match expected structure."""
+        # TODO: Delete this method if not needed.
+        return row
